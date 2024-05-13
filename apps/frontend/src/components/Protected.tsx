b87a3ee7109117c7e5ab6fb2c19c43c50c2d5430
Leonardo Figueiredo <leonardo.figueiredo@ipflagship.com.br>
2024-05-13 11:54:19 -0300

    fix: adjust keycloak callback

--- apps/frontend/src/components/Protected.tsx
+++ apps/frontend/src/components/Protected.tsx
@@ -1,14 +1,4 @@
-import { useEffect, useRef } from "react";
-
 const Protected = () => {
-  const isRun = useRef(false);
-
-  useEffect(() => {
-    if (isRun.current) return;
-
-    isRun.current = true;
-  }, []);
-
   return <div>Protected</div>;
 };
 
---
+++ apps/frontend/src/security/KeycloakService.ts
@@ -0,0 +1,29 @@
+import Keycloak from "keycloak-js";
+
+const keycloakInstance = new Keycloak();
+
+/**
+ * Initializes Keycloak instance and calls the provided callback function if successfully authenticated.
+ *
+ * @param onAuthenticatedCallback
+ */
+const Login = (onAuthenticatedCallback: () => void) => {
+  keycloakInstance
+    .init({ onLoad: "login-required", pkceMethod: "S256" })
+    .then(function (authenticated) {
+      authenticated ? onAuthenticatedCallback() : alert("non authenticated");
+    })
+    .catch((e) => {
+      console.dir(e);
+      console.log(`keycloak init exception: ${e}`);
+    });
+};
+
+const UserName = () => keycloakInstance.tokenParsed?.preferred_username;
+
+const KeyCloakService = {
+  CallLogin: Login,
+  GetUserName: UserName,
+};
+
+export default KeyCloakService;
